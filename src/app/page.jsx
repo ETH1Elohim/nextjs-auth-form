c0bbc078b41a0775ddc76835da5d009fbef7622a
ETH1Elohim <ezmax.defi@gmail.com>
2023-08-28 10:58:26 -0500

    formik & yup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,27 @@
 import Image from 'next/image';
 import CountrySelect from './components/country-dropdown/page';
 import { useState } from 'react';
+import formImage from '../../public/SimpFi-Splash-Page.png';
+import { useFormik } from 'formik';
 
 export default function Home() {
+  
   const [country, setCountry] = useState("");
 
   const onCountryChange = (selectedCountry) => {
     setCountry(selectedCountry);
   }
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      country: 'United States',
+      terms: "",
+    }
+  })
+
+  console.log(formik.values);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 m-auto">
@@ -20,30 +34,44 @@ export default function Home() {
 
             {/* Name input field */}
             <div className='pb-4 flex flex-col'>
-              <label htmlFor='name' className='px-1'>Name</label>
+              <label htmlFor='name' className='px-1 font-semibold'>Name</label>
               <input 
+              className='border-2 border-black rounded-md m-1 px-1 py-px w-1/2'
               type="text" 
               name='name' 
-              placeholder='Enter your name' className='border-2 border-black rounded-md m-1 px-1 py-px w-1/2' />
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              placeholder='Enter your name' 
+               />
             </div>
 
             {/* Email input field */}
             <div className='pb-4 flex flex-col'>
-              <label htmlFor='email' className='px-1'>Email</label>
+              <label htmlFor='email' className='px-1 font-semibold'>Email</label>
               <input 
               type="email" 
               name='email' 
+              onChange={(e) => setEmail(e.target.value)}
               placeholder='Enter your email' className='border-2 border-black rounded-md m-1 px-1 py-px w-1/2' />
             </div>
 
             {/* Country input field */}
             <div className='pb-4 flex flex-col'>
-              <label htmlFor='country' className='px-1'>Country</label>
+              <label htmlFor='country' className='px-1 font-semibold'>Country</label>
               <CountrySelect onCountryChange={onCountryChange} />
             </div>
 
+            {/* Terms of Service */}
+            <div className='pb-4 flex flex-col'>
+              <label htmlFor='terms' className='px-1 font-semibold'>Terms of service</label>
+              <div className='flex items-center'>
+                <input type="checkbox" name='terms' value='checked' className='h-5 w-5 rounded-md border-2 border-black m-1' /><p>I have read agree to the Terms of service.</p> 
+              </div>
+            </div>
+            <button type='submit' className='bg-black text-white text-md py-3 mt-6 rounded-md w-full'>Get Early Access</button>
           </div>
         </div>
+
       </form>
     </main>
   )
